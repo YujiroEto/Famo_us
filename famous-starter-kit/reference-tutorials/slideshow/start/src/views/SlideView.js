c2acdf16891fe3a2db42e69c0da470efc586c5d4
Yujiro Eto <yujro.eto@gmail.com>
2014-06-08 18:56:02 +0900

    mod

---
+++ famous-starter-kit/reference-tutorials/slideshow/start/src/views/SlideView.js
@@ -0,0 +1,116 @@
+/*** SlideView.js ***/
+
+define(function(require, exports, module) {
+	var View = require('famous/core/View');
+	var Surface = require('famous/core/Surface');
+	var Transform = require('famous/core/Transform');
+	var StateModifier = require('famous/modifiers/StateModifier');
+    var ImageSurface = require('famous/surfaces/ImageSurface');
+	
+	var SlideData = require('data/SlideData');
+
+	SlideView.DEFAULT_OPTIONS = {
+    	size: [400, 450],
+		filmBorder: 15,
+		photoBorder: 3,
+		photoUrl: SlideData.defaultImage
+	};
+
+	var film = new Surface({
+		size: [this.options.filmSize, this.options.filmSize],
+		properties: {
+			backgroundColor: '#222',
+			zIndex: 1,
+		// makes the surface invisible to clicks
+		pointerEvents: 'none'
+		}
+	});
+
+	var photo = new ImageSurface({
+		size: [size, size],
+		content: this.options.photoUrl,
+		properties: {
+			zIndex: 2,
+			pointerEvents: 'none'
+		}
+	});
+
+	function SlideView() {
+		View.apply(this, arguments);
+
+		this.rootModifier = new StateModifier({
+			size: [400, 450]
+		});
+
+		this.mainNode = this.add(this.rootModifier);
+
+		// make sure you invoke the helper function
+		// in the right context by using .call()
+		_createBackground.call(this);
+		_createFilm.call(this);
+		_createPhoto.call(this);
+	}
+
+	// the _ before the function name indicates it's a private function
+	function _createBackground() {
+		var background = new Surface({
+			properties: {
+				backgroundColor: '#FFFFF5',
+			boxShadow: '0 10px 20px -5px rgba(0, 0, 0, 0.5)'
+			}
+		});
+
+		this.mainNode.add(background);
+
+		background.on('click', function() {
+			// the event output handler is used to broadcast outwards
+			this._eventOutput.emit('click');
+		}.bind(this));
+	}
+
+	function _createFilm() {
+		this.options.filmSize = this.options.size[0] - 2 * this.options.filmBorder;
+
+		var film = new Surface({
+			size: [this.options.filmSize, this.options.filmSize],
+			properties: {
+				backgroundColor: '#222',
+			zIndex: 1
+			}
+		});
+
+		var filmModifier = new StateModifier({
+			origin: [0.5, 0],
+			align: [0.5, 0],
+			transform: Transform.translate(0, this.options.filmBorder, 1)
+		});
+
+		this.mainNode.add(filmModifier).add(film);
+	}
+
+	function _createPhoto() {
+		var photoSize = this.options.filmSize - 2 * this.options.photoBorder;
+
+		var photo = new ImageSurface({
+			size: [photoSize, photoSize],
+			content: this.options.photoUrl,
+			properties: {
+				zIndex: 2
+			}
+		});
+
+		this.photoModifier = new StateModifier({
+			origin: [0.5, 0],
+			align: [0.5, 0],
+			transform: Transform.translate(0, this.options.filmBorder + this.options.photoBorder, 2)
+		});
+
+		this.mainNode.add(this.photoModifier).add(photo);
+	}
+
+
+
+	SlideView.prototype = Object.create(View.prototype);
+	SlideView.prototype.constructor = SlideView;
+	module.exports = SlideView;
+});
